84a73840dccf6db6fe6b5bfc01e5a3e5c97ca499
Mat Evans <mat@coull.com>
2014-03-27 14:27:33 +0000

    fix some errors in http

--- http/http.go
+++ http/http.go
@@ -1,6 +1,9 @@
-package metrics
+package http
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/matzhouse/go-metrics"
 	"net/http"
 	"runtime"
 )
@@ -21,7 +24,7 @@ type httpmetric struct {
 	Value string
 }
 
-func Http() {
+func Http(r metrics.Registry) {
 
 	port := "127.0.0.1:6007"
 
@@ -32,49 +35,48 @@ func Http() {
 
 }
 
-func metricshandler(w http.ResponseWriter, r *http.Request) {
+func metricshandler(writer http.ResponseWriter, request *http.Request) {
 
-	m := data()
+	m := data(r)
 
 	o, err := json.Marshal(m)
 
 	if err != nil {
-		fmt.Fprintf(w, "Error in Marshalling JSON")
+		fmt.Fprintf(writer, "Error in Marshalling JSON")
 	} else {
-		fmt.Fprintf(w, "%s", o)
+		fmt.Fprintf(writer, "%s", o)
 	}
 
 }
 
-func data() (output httpmetrics) {
-
-	series := *[]httpmetric
+func data(r metrics.Registry) (output httpmetrics) {
 
 	r.Each(func(name string, i interface{}) {
 		now := getCurrentTime()
 		switch metric := i.(type) {
 		case metrics.Counter:
 
-			series = append(series, &metric{"counter", metric.Count()})
+			output.metrics = append(series, &metric{"counter", metric.Count()})
 
 		case metrics.Gauge:
-			series = append(series, &influxClient.Series{
-				Name:    fmt.Sprintf("%s.value", name),
-				Columns: []string{"time", "value"},
-				Points: [][]interface{}{
-					{now, metric.Value()},
-				},
-			})
+			/*
+				series = append(series, &influxClient.Series{
+					Name:    fmt.Sprintf("%s.value", name),
+					Columns: []string{"time", "value"},
+					Points: [][]interface{}{
+						{now, metric.Value()},
+					},
+				})*/
 		case metrics.GaugeFloat64:
-			series = append(series, &influxClient.Series{
+			/*series = append(series, &influxClient.Series{
 				Name:    fmt.Sprintf("%s.value", name),
 				Columns: []string{"time", "value"},
 				Points: [][]interface{}{
 					{now, metric.Value()},
 				},
-			})
+			})*/
 		case metrics.Histogram:
-			h := metric.Snapshot()
+			/*h := metric.Snapshot()
 			ps := h.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
 			series = append(series, &influxClient.Series{
 				Name: fmt.Sprintf("%s.histogram", name),
@@ -85,9 +87,9 @@ func data() (output httpmetrics) {
 					{now, h.Count(), h.Min(), h.Max(), h.Mean(), h.StdDev(),
 						ps[0], ps[1], ps[2], ps[3], ps[4]},
 				},
-			})
+			})*/
 		case metrics.Meter:
-			m := metric.Snapshot()
+			/*m := metric.Snapshot()
 			series = append(series, &influxClient.Series{
 				Name: fmt.Sprintf("%s.meter", name),
 				Columns: []string{"count", "one-minute",
@@ -96,20 +98,23 @@ func data() (output httpmetrics) {
 					{m.Count(), m.Rate1(), m.Rate5(), m.Rate15(), m.RateMean()},
 				},
 			})
+			*/
 		case metrics.Timer:
-			h := metric.Snapshot()
-			ps := h.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
-			series = append(series, &influxClient.Series{
-				Name: fmt.Sprintf("%s.timer", name),
-				Columns: []string{"count", "min", "max", "mean", "std-dev",
-					"50-percentile", "75-percentile", "95-percentile",
-					"99-percentile", "999-percentile", "one-minute", "five-minute", "fifteen-minute", "mean-rate"},
-				Points: [][]interface{}{
-					{h.Count(), h.Min(), h.Max(), h.Mean(), h.StdDev(),
-						ps[0], ps[1], ps[2], ps[3], ps[4],
-						h.Rate1(), h.Rate5(), h.Rate15(), h.RateMean()},
-				},
-			})
+			/*
+				h := metric.Snapshot()
+				ps := h.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
+				series = append(series, &influxClient.Series{
+					Name: fmt.Sprintf("%s.timer", name),
+					Columns: []string{"count", "min", "max", "mean", "std-dev",
+						"50-percentile", "75-percentile", "95-percentile",
+						"99-percentile", "999-percentile", "one-minute", "five-minute", "fifteen-minute", "mean-rate"},
+					Points: [][]interface{}{
+						{h.Count(), h.Min(), h.Max(), h.Mean(), h.StdDev(),
+							ps[0], ps[1], ps[2], ps[3], ps[4],
+							h.Rate1(), h.Rate5(), h.Rate15(), h.RateMean()},
+					},
+				})
+			*/
 		}
 		if err := client.WriteSeries(series); err != nil {
 			log.Println(err)
